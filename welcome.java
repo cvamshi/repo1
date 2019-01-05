ba674228a40c73dc938cace5fa345a9a2104db8a
cvamshi <46273986+cvamshi@users.noreply.github.com>
2019-01-05 13:47:05 +0530

    welcome.java
    1st java file committed

---
+++ welcome.java
@@ -0,0 +1,11 @@
+class Test{
+int a;
+int b;
+public void m1(){System.out.println("hello welcome to java");}
+public static void main(String args[])
+{
+Test t=new Test();
+t.m1();
+System.out.println("welcome to main method");
+}
+}
